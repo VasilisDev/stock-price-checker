78970bc46eaa336e0ffdbfcefd87b690b316ba4a
VasilisDev <37515493+VasilisDev@users.noreply.github.com>
2019-02-15 23:16:18 +0200

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,11 @@
 /*
 *
 *
-*       FILL IN EACH FUNCTIONAL TEST BELOW COMPLETELY
-*       -----[Keep the tests in the same order!]-----
-*       (if additional are added, keep them at the very end!)
+*       @Author:Vasileios Tsakiris
+*
+*
 */
+
 
 var chaiHttp = require('chai-http');
 var chai = require('chai');
